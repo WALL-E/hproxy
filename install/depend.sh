f2052ab24f8e7b62b05b8218cc53272ad3d444df
zhangzheng <zhangzheng@qianbao.com>
2017-01-11 18:04:43 +0800

    增加安装脚本

---
+++ install/depend.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+role=`id -u`
+if test $role -ne 0
+then
+    echo "运行脚本需要root权限"
+    exit 1
+fi
+
+pip2.7 install http://oerp142a4.bkt.clouddn.com/beautifulsoup4-4.5.3.tar.gz
+pip2.7 install http://oerp142a4.bkt.clouddn.com/ipaddress-1.0.17.tar.gz
