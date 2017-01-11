0147f189f4e15968218f869518a7d8fa7fc8c160
zhangzheng <zhangzheng@qianbao.com>
2017-01-11 17:00:33 +0800

    scrapy初始化

---
+++ hproxy/spiders/com_kxdaili.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from bs4 import BeautifulSoup
+from hproxy.items import HproxyItem
+
+
+class ComKxdailiSpider(scrapy.Spider):
+    name = "com_kxdaili"
+    allowed_domains = ["kxdaili.com"]
+    start_urls = ['http://www.kxdaili.com/dailiip.html']
+
+    def parse(self, response):
+        soup = BeautifulSoup(response.text, "html.parser")
+        tables = soup.findAll('table')
+        table = tables[0]
+        for tr in table.findAll('tr')[1:]:
+            item = HproxyItem()
+            tds = tr.findAll('td')
+            if len(tds) > 0:
+                item["ip"] = tds[0].string
+                item["port"] = tds[1].string
+                yield item
