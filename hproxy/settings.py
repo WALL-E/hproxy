f78aeeaa76b6275fb00de6a76bece89fd6b01d12
zhangzheng <zhangzheng@qianbao.com>
2017-01-11 18:02:47 +0800

    增加持久化存储配置

--- hproxy/settings.py
+++ hproxy/settings.py
@@ -88,3 +88,6 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+FEED_URI = 'file:///tmp/feeds/%(name)s/%(time)s.csv'
+FEED_FORMAT = 'csv'
